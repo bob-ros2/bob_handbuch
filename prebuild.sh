1f23c177f3897bb6a86376dfbfd410b2a22ad2d6
Bob Ros <bob-ros@t-online.de>
2024-11-08 02:57:24 +0100

    add prebuild script

---
+++ prebuild.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+mkdir -p doc/bob
+curl https://raw.githubusercontent.com/bob-ros2/bob_whisper_cpp/refs/heads/main/README.md > doc/bob/bob-whisper-cpp.md
+curl https://raw.githubusercontent.com/bob-ros2/rosgpt4all/refs/heads/main/README.md > doc/bob/rosgpt4all.md
+curl https://raw.githubusercontent.com/bob-ros2/bob_topic_tools/refs/heads/main/README.md > doc/bob/bob-topic-tools.md
+curl https://raw.githubusercontent.com/bob-ros2/voskros/refs/heads/main/README.md > doc/bob/voskros.md
+
+
+cat <<EOF > doc/index.rst
+.. You can adapt this file completely to your liking, but it should at least
+   contain the root toctree directive.
+
+Welcome to Bobs's Handbuch!
+===================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Packages:
+
+   bob/bob-topic-tools.md
+   bob/bob-whisper-cpp.md
+   bob/rosgpt4all.md
+   bob/voskros.md
+
+EOF
